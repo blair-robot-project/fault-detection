97f5b83273f84a6a3ed1071e6a56215538a75183
Bryan Li <bryanli0@me.com>
2018-01-16 16:31:07 -0500

    Added data and created code to display data.

--- server.R
+++ server.R
@@ -9,22 +9,30 @@
 
 library(shiny)
 library(dplyr)
+library(ggplot2)
 
-#file <- read.csv("logs/telemetryLog-1969.12.31.20.51.15.csv")
-#time <- file[-1,1]
-#values <- file[-1,2]
+file <- read.csv("logs/telemetryLog-1969.12.31.20.51.15.csv", header=TRUE)
+file <- file[c(1,which(rowSums(diff(as.matrix(file[,grep('time',names(file))])))!=0)+1),]
+logNames <- names(file)
+logTimes <- file[-1,1]
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
    
   output$distPlot <- renderPlot({
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    # get data based on input$dataVal from ui.R
     
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    xData <- logTimes
+    yData <- file[-1, input$dataVal]
+    name <- input$dataVal
+    curData <- data.frame(x, y)
+    
+    
+    # draw plot with specified data values
+    if (input$plotType == "smooth") ggplot(curData, aes(x=xData, y=yData)) + geom_point() + geom_smooth(method = "loess") + labs(x = "Time", y = name)
+    else (ggplot(curData, aes(x=xData, y=yData)) + geom_line() + labs(x = "Time", y = name))
+    #qplot(data=curData)
     
   })
   
--- ui.R
+++ ui.R
@@ -8,21 +8,19 @@
 #
 
 library(shiny)
-
+logNames <- names(read.csv("logs/telemetryLog-1969.12.31.20.51.15.csv", header=TRUE)[,-1])
+sliderLabel <- "Data Type"
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Robot Fault Detection"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+      selectInput("dataVal", label = "Data Type", logNames),
+      selectInput("plotType", label = "Plot Type", c("Smoothed Conditional + Scatterplot" = "smooth", "Line plot" = "line"))
     ),
     
     # Show a plot of the generated distribution
